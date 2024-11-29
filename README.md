f77f4f2a23745e15e23e69a5e5c66e7f3e54a8c3
ezhilsabareesh8 <ezhilsabareesh@gmail.com>
2024-11-29 12:22:40 +1100

    Add License and pass values

--- README.md
+++ README.md
@@ -1,12 +1,23 @@
 # make_OM3_025deg_topo
 
-Make 0.25 degree topog.nc MOM bathymetry file from GEBCO 2024 dataset.
+Make 0.25-degree `topog.nc` MOM bathymetry file from the GEBCO 2024 dataset.
 
-./gen_topog.sh will generate topography and associated files. For 0.25deg resolution and higher this will require qsub.
+## Workflow Overview
 
-When the output files are to your satisfaction, commit and push your changes and add the git commit hash as metadata in the output .nc files by running finalise.sh.
+1. **Generate Topography:**  
+   Run `./gen_topog.sh` to generate the topography and associated files. For 0.25-degree resolution or higher, this will require submission via `qsub`.
 
-Use the qsub command to submit the script, passing the required input files as arguments, For example:
+2. **Finalize Output Files:**  
+   Once the output files meet your satisfaction, commit and push the changes, then add the git commit hash as metadata in the output `.nc` files by running `finalise.sh`.
 
-qsub -v INPUT_HGRID="/path/to/ocean_hgrid.nc",INPUT_VGRID="/path/to/ocean_vgrid.nc",INPUT_GBCO="/path/to/GEBCO_2024.nc" gen_topo.sh
+3. **Submitting the Script:**  
+   Use the `qsub` command to submit the script, passing the required input files as arguments. For example:  
+   ```bash
+   qsub -v INPUT_HGRID="/path/to/ocean_hgrid.nc",INPUT_VGRID="/path/to/ocean_vgrid.nc",INPUT_GBCO="/path/to/GEBCO_2024.nc" gen_topo.sh
+   ```
 
+## Note on Dependencies  
+
+This workflow relies on the **hh5 conda environments** for running the scripts and generating the outputs. Ensure that the required conda environment is active before executing the workflow.
+
+--- 
--- gen_topo.sh
+++ gen_topo.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env sh
+# Copyright 2023 ACCESS-NRI and contributors. See the top-level COPYRIGHT file for details.
+# SPDX-License-Identifier: Apache-2.0
+
 #PBS -q normal
 #PBS -l walltime=4:00:00,mem=4GB
 #PBS -l wd
@@ -23,6 +26,10 @@ set -e
 INPUT_HGRID=$INPUT_HGRID
 INPUT_VGRID=$INPUT_VGRID
 INPUT_GBCO=$INPUT_GBCO
+# Define the cutoff value
+CUTOFF_VALUE=6000
+ESMF_MESH_FILE='access-om3-025deg-ESMFmesh.nc'
+ESMF_NO_MASK_MESH_FILE='access-om3-025deg-nomask-ESMFmesh.nc' 
 
 # Copy and link input files
 cp -L --preserve=timestamps "$INPUT_HGRID" ./ocean_hgrid.nc
@@ -36,13 +43,13 @@ ln -sf "$INPUT_GBCO" ./GEBCO_2024.nc
 ./domain-tools/bin/topogtools gen_topo -i GEBCO_2024.nc -o topog_new.nc --hgrid ocean_hgrid.nc --tripolar --longitude-offset -100 
 
 # Cut off T cells of size less than 6kms (6000 m)
-./domain-tools/bin/topogtools min_dy -i topog_new.nc -o topog_new_min_dy.nc --cutoff 6000 --hgrid ocean_hgrid.nc
+./domain-tools/bin/topogtools min_dy -i topog_new.nc -o topog_new_min_dy.nc --cutoff "$CUTOFF_VALUE" --hgrid ocean_hgrid.nc
 
 # Fill cells that have a sea area fraction smaller than 0.5:
 ./domain-tools/bin/topogtools fill_fraction -i topog_new_min_dy.nc -o topog_new_fillfraction.nc  --fraction 0.5
 
 # edit_topo.py
-python ./topogtools/editTopo.py --overwrite --nogui --apply edit_topog_new_fillfraction.txt --output topog_new_fillfraction_edited.nc topog_new_fillfraction.nc
+python ./topogtools/editTopo.py --overwrite --nogui --apply edit_025deg_topog_new_fillfraction.txt --output topog_new_fillfraction_edited.nc topog_new_fillfraction.nc
 
 # Remove seas:
 ./domain-tools/bin/topogtools deseas -i topog_new_fillfraction_edited.nc -o topog_new_fillfraction_edited_deseas.nc --grid_type C
@@ -70,19 +77,19 @@ ncatted -O -h -a md5_checksum,global,a,c,"$MD5SUM_mask" ocean_mask.nc
 ncrename -O -v mask,kmt ocean_mask.nc kmt.nc 
 
 #Move intermediate files to a separate directory
-OUTPUT_DIR="topography_output"
+OUTPUT_DIR="topography_intermediate_output"
 mkdir -p $OUTPUT_DIR
 
 # Move all intermediate files to topography_output directory
 mv topog_new.nc $OUTPUT_DIR/
 mv topog_new_min_dy.nc $OUTPUT_DIR/
-mv topog_new_fillfraction.nc $OUTPUT_DIR/
+mv topog_new_fillfraction.nc $OUTPUT_DIR/  
 mv topog_new_fillfraction_edited.nc $OUTPUT_DIR/
 mv topog_new_fillfraction_edited_deseas.nc $OUTPUT_DIR/
 mv topog_new_fillfraction_edited_deseas_mindepth.nc $OUTPUT_DIR/
 
 # Create ESMF mesh from hgrid and ocean_mask.nc
-python3 ./om3-scripts/mesh_generation/generate_mesh.py --grid-type=mom --grid-filename=ocean_hgrid.nc --mesh-filename=access-om3-025deg-ESMFmesh.nc --mask-filename=ocean_mask.nc --wrap-lons
+python3 ./om3-scripts/mesh_generation/generate_mesh.py --grid-type=mom --grid-filename=ocean_hgrid.nc --mesh-filename="$ESMF_MESH_FILE" --mask-filename=ocean_mask.nc --wrap-lons
 
 # Create ESMF mesh without mask
-python3 ./om3-scripts/mesh_generation/generate_mesh.py --grid-type=mom --grid-filename=ocean_hgrid.nc --mesh-filename=access-om3-025deg-nomask-ESMFmesh.nc --wrap-lons
+python3 ./om3-scripts/mesh_generation/generate_mesh.py --grid-type=mom --grid-filename=ocean_hgrid.nc --mesh-filename="$ESMF_NO_MASK_MESH_FILE" --wrap-lons
