a53bfaf6a4d77008a0aaaab0ff850c0154f0ce18
anton-seaice <anton.steketee@anu.edu.au>
2024-12-05 09:35:06 +1100

    update

--- gen_topo.sh
+++ gen_topo.sh
@@ -57,6 +57,13 @@ python ./topogtools/editTopo.py --overwrite --nogui --apply edit_025deg_topog_ne
 
 # Name final topog as topo.nc
 cp topog_new_fillfraction_edited_deseas_mindepth.nc topog.nc
+# add name and checksum for input files
+MD5SUM=$(md5sum ocean_hgrid.nc | awk '{print $1}')
+ncatted -O -h -a input_file,global,a,c,"$(readlink -f $INPUT_HGRID) (md5sum:$MD5SUM) ; " topog.nc
+MD5SUM=$(md5sum ocean_vgrid.nc | awk '{print $1}')
+ncatted -O -h -a input_file,global,a,c,"$(readlink -f $INPUT_VGRID) (md5sum:$MD5SUM) ; " topog.nc
+MD5SUM=$(md5sum GEBCO_2024.nc | awk '{print $1}')
+ncatted -O -h -a input_file,global,a,c,"$(readlink -f $INPUT_GBCO) (md5sum:$MD5SUM) ; " topog.nc
 
 #Move intermediate files to a separate directory
 OUTPUT_DIR="topography_intermediate_output"
@@ -68,14 +75,15 @@ mv topog_new* $OUTPUT_DIR/
 
 # Add MD5 checksum as a global attribute to topog.nc
 MD5SUM_topog=$(md5sum topog.nc | awk '{print $1}')
-ncatted -O -h -a md5_checksum,global,a,c,"$MD5SUM_topog" ocean_mask.nc
+ncatted -O -h -a input_file,global,a,c,"$(readlink -f topog.nc) (md5sum:$MD5SUM_topog)" ocean_mask.nc
 
 # Make CICE mask file (`kmt.nc`) 
 ncrename -O -v mask,kmt ocean_mask.nc kmt.nc 
+ncks -O -x -v geolon_t,geolat_t kmt.nc kmt.nc #drop unused vars
 
 # Add MD5 checksum as a global attribute to ocean_mask.nc
 MD5SUM_mask=$(md5sum ocean_mask.nc | awk '{print $1}')
-ncatted -O -h -a md5_checksum,global,a,c,"$MD5SUM_mask" kmt.nc
+ncatted -O -h -a ocean_mask_file,global,a,c,"$(readlink -f ocean_mask.nc) (md5sum:$MD5SUM_mask)" kmt.nc
 
 # Create ESMF mesh from hgrid and ocean_mask.nc
 python3 ./om3-scripts/mesh_generation/generate_mesh.py --grid-type=mom --grid-filename=ocean_hgrid.nc --mesh-filename="$ESMF_MESH_FILE" --mask-filename=ocean_mask.nc --wrap-lons
