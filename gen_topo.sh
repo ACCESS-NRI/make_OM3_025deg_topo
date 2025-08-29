49f2606cd8f2b4335ea8d89f8f59a416ea12af6d
minghang.li <24727729+minghangli-uni@users.noreply.github.com>
2025-08-29 15:13:19 +1000

    ensure md5 of vgrid using the original double-precison vgrid (#15)
    ---------
    
    Co-authored-by: Anton Steketee <79179784+anton-seaice@users.noreply.github.com>

--- gen_topo.sh
+++ gen_topo.sh
@@ -58,11 +58,11 @@ python3 ./bathymetry-tools/editTopo.py --overwrite --nogui --apply edit_025deg_t
 # Name final topog as topo.nc
 cp topog_new_fillfraction_edited_deseas_mindepth.nc topog.nc
 # add name and checksum for input files
-MD5SUM=$(md5sum ocean_hgrid.nc | awk '{print $1}')
+MD5SUM=$(md5sum $INPUT_HGRID | awk '{print $1}')
 ncatted -O -h -a input_file,global,a,c,"$(readlink -f $INPUT_HGRID) (md5sum:$MD5SUM) ; " topog.nc
-MD5SUM=$(md5sum ocean_vgrid.nc | awk '{print $1}')
+MD5SUM=$(md5sum "$INPUT_VGRID" | awk '{print $1}')
 ncatted -O -h -a input_file,global,a,c,"$(readlink -f $INPUT_VGRID) (md5sum:$MD5SUM) ; " topog.nc
-MD5SUM=$(md5sum GEBCO_2024.nc | awk '{print $1}')
+MD5SUM=$(md5sum $INPUT_GBCO | awk '{print $1}')
 ncatted -O -h -a input_file,global,a,c,"$(readlink -f $INPUT_GBCO) (md5sum:$MD5SUM) ; " topog.nc
 
 #Move intermediate files to a separate directory
